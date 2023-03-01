c5cef243abf328be435adda70fbc2755c00d8419
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-01 20:59:02 +0300

    Got rid of all the unused imports

--- src/components/NavBar/SearchInput.tsx
+++ src/components/NavBar/SearchInput.tsx
@@ -1,6 +1,5 @@
 import { SearchIcon } from '@chakra-ui/icons'
-import { Flex, Input, InputGroup, InputLeftElement, InputRightElement } from '@chakra-ui/react'
-import React from 'react'
+import { Flex, Input, InputGroup, InputLeftElement } from '@chakra-ui/react'
 
 type Props = {
 
