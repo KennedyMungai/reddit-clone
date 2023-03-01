0d91e05d87a90c6a7cd96fed5226b15ef9c7b7b2
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-01 20:49:58 +0300

    Added some code and updated all the imports

--- src/components/NavBar/SearchInput.tsx
+++ src/components/NavBar/SearchInput.tsx
@@ -1,4 +1,4 @@
-import { Flex } from '@chakra-ui/react'
+import { Flex, Input, InputGroup, InputLeftElement, InputRightElement } from '@chakra-ui/react'
 import React from 'react'
 
 type Props = {
@@ -9,7 +9,24 @@ const SearchInput = (props: Props) =>
 {
     return (
         <Flex>
+            <InputGroup>
+                <InputLeftElement
+                    pointerEvents='none'
+                    children={<PhoneIcon color='gray.300' />}
+                />
+                <Input type='tel' placeholder='Phone number' />
+            </InputGroup>
 
+            <InputGroup>
+                <InputLeftElement
+                    pointerEvents='none'
+                    color='gray.300'
+                    fontSize='1.2em'
+                    children='$'
+                />
+                <Input placeholder='Enter amount' />
+                <InputRightElement children={<CheckIcon color='green.500' />} />
+            </InputGroup>
         </Flex>
     )
 }
