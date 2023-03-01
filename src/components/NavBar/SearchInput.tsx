0d712ea11231001e4af2ffb647403eee9ae4ad2c
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-01 20:45:45 +0300

    Created the skeleton of the Seatch input component

---
+++ src/components/NavBar/SearchInput.tsx
@@ -0,0 +1,17 @@
+import { Flex } from '@chakra-ui/react'
+import React from 'react'
+
+type Props = {
+
+}
+
+const SearchInput = (props: Props) =>
+{
+    return (
+        <Flex>
+
+        </Flex>
+    )
+}
+
+export default SearchInput
