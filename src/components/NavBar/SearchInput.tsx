0cb6e6c825a8631fb231f45f224b8417036fd660
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-01 20:59:50 +0300

    Changed the placeholdr text to read 'Search Reddit'

--- src/components/NavBar/SearchInput.tsx
+++ src/components/NavBar/SearchInput.tsx
@@ -14,7 +14,7 @@ const SearchInput = () =>
                     pointerEvents='none'
                     children={<SearchIcon color='gray.300' />}
                 />
-                <Input type='tel' placeholder='Search Posts' />
+                <Input type='tel' placeholder='Search Reddit' />
             </InputGroup>
         </Flex>
     )
