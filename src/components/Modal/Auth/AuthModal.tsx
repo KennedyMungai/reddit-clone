8141e764697b4f73b221a8a23c2c8c34dd120949
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-02 04:43:33 +0300

    Added the content of the AuthModal component

--- src/components/Modal/Auth/AuthModal.tsx
+++ src/components/Modal/Auth/AuthModal.tsx
@@ -1,10 +1,39 @@
+import { Button, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay } from '@chakra-ui/react'
 import React from 'react'
 
 const AuthModal = () =>
 {
     return (
         <>
+            <Button>Open Modal</Button>
+            <Modal
+                isOpen={isOpen}
+                onClose={onClose}
+            >
+                <ModalOverlay />
+                <ModalContent>
+                    <ModalHeader>Modal Title</ModalHeader>
+                    <ModalCloseButton />
+                    <ModalBody>
+                        <Lorem count={2} />
+                    </ModalBody>
 
+                    <ModalFooter>
+                        <Button
+                            colorScheme={'blue'}
+                            mr={3}
+                            onClick={onClose}
+                        >
+                            Close
+                        </Button>
+                        <Button
+                            variant={'ghost'}
+                        >
+                            Secondary Action
+                        </Button>
+                    </ModalFooter>
+                </ModalContent>
+            </Modal>
         </>
     )
 }
