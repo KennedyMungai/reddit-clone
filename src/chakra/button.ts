4bb7b1bcdac4535643db83905ace7d4efeac41be
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-02 04:10:13 +0300

    Created the component style config element

--- src/chakra/button.ts
+++ src/chakra/button.ts
@@ -1,1 +1,9 @@
 import { ComponentStyleConfig } from "@chakra-ui/react";
+
+
+export const Button: ComponentStyleConfig = {
+    baseStyle: {
+        borderRadius: '60px',
+        color: 'brand.100'
+    }
+}
