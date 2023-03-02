9399f5717f9ac0a5bf66aa893c8de5d15e0eb191
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-02 04:23:13 +0300

    Gave the Log In button a barianmt of outline

--- src/components/NavBar/RightContent/AuthButtons.tsx
+++ src/components/NavBar/RightContent/AuthButtons.tsx
@@ -5,7 +5,7 @@ const AuthButtons = () =>
 {
     return (
         <>
-            <Button>Log In</Button>
+            <Button variant='outline'>Log In</Button>
             <Button>Sign Up</Button>
         </>
     )
