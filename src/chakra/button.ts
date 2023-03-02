00b9918ce2677e8a2fcb911f86abf0efc70a28f8
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-02 04:12:59 +0300

    Gave the buttons a thin red border

--- src/chakra/button.ts
+++ src/chakra/button.ts
@@ -4,6 +4,7 @@ import { ComponentStyleConfig } from "@chakra-ui/react";
 export const Button: ComponentStyleConfig = {
     baseStyle: {
         borderRadius: '60px',
-        color: 'brand.100'
+        color: 'brand.100',
+        border: '0.05rem solid red'
     }
 }
